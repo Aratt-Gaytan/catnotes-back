66776361af141aaeac254f246bf2dcf55e603c47
Aratt-Gaytan <aratt.gaytan.flores0@gmail.com>
2024-11-12 18:47:24 -0600

    cors

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 // server.js
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); // Corrección aquí
 const Database = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const scheduleRoutes = require('./routes/scheduleRoutes');
-cosnt Cors = require("cors")
 
 const app = express();
-app.use(Cors())
 const PORT = process.env.PORT || 5000;
 
+// Middleware
+app.use(cors()); // Uso correcto de CORS
+app.use(express.json());
+
+// Conectar a la base de datos
 Database.connect();
 
-app.use(express.json());
+// Rutas
 app.use('/api/user', userRoutes);
 app.use('/api/schedule', scheduleRoutes);
 
+// Iniciar servidor
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
