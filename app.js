168a95e568b714ff7d9ac0028f379876a74a5986
Aratt-Gaytan <aratt.gaytan.flores0@gmail.com>
2024-11-14 18:16:09 -0600

    notifications new

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors'); // Corrección aquí
 const Database = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const scheduleRoutes = require('./routes/scheduleRoutes');
+const notificationRouter = require('./routes/notificationRouter');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,6 +20,7 @@ Database.connect();
 // Rutas
 app.use('/api/user', userRoutes);
 app.use('/api/schedule', scheduleRoutes);
+app.use('/api/notification', notificationRouter);
 
 // Iniciar servidor
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- routes/notificationRouter.js
+++ routes/notificationRouter.js
@@ -1,3 +1,5 @@
+const admin = require('firebase-admin');
+
 router.post('/schedule/notify', validateToken, async (req, res) => {
   const { userId, notificationType } = req.body;
 
@@ -9,3 +11,26 @@ router.post('/schedule/notify', validateToken, async (req, res) => {
     res.status(500).json({ error: 'Error al enviar la notificación' });
   }
 });
+
+router.post('/send-notification', async (req, res) => {
+  const { token, title, body } = req.body;
+
+  const message = {
+    to: token,
+    notification: {
+      title: title,
+      body: body,
+    },
+  };
+
+  try {
+    await admin.messaging().send(message);
+    res.json({ message: 'Notificación enviada' });
+  } catch (error) {
+    console.error('Error sending message:', error);
+    res.status(500).send('Error');
+  }
+});
+
+
+module.exports = router;
